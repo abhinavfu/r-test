b2be8fe6934b40acb5b7d255c6d8beb6b4f6bbdc
Abhinav Anand <aabhinavfu007@gmail.com>
2024-10-01 22:43:44 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import env from "react-dotenv";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello Test
+      <div>
+        <p>env : {env.API_KEY}</p>
+        <p>window env : {window.env.API_KEY}</p>
+      </div>
     </div>
   );
 }
